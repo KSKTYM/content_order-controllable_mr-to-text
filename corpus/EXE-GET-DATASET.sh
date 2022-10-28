906eae1f996cedc21032b190ab5c1eeb487a6e76
KSKTYM <keisuke.toyama@gmail.com>
2022-10-29 02:30:37 +0900

    E2E refined dataset v.0.9.0

--- corpus/EXE-GET-DATASET.sh
+++ corpus/EXE-GET-DATASET.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # download E2E refined dataset (distrubited under the CC4.0-BY-SA license)
-curl -OL https://github.com/KSKTYM/E2E-refined-dataset/release/e2e_refined_dataset_v1_0_0.zip
-unzip e2e_refined_dataset_v1_0_0.zip
+curl -OL https://github.com/KSKTYM/E2E-refined-dataset/blob/main/release/e2e_refined_dataset_v0_9_0.zip
+unzip e2e_refined_dataset_v0_9_0.zip
 mv release/e2e_refined_dataset .
 rmdir -fr release
